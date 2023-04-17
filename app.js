f219ef41a67fcc5bfb8e3ec00c6b60938aafdade
Florian PESCOT-JOURDAN <275@dupuy-agri.com>
2023-04-17 11:23:21 +0200

    updateBDD

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const odbc = require('odbc');
 
 const app = express();
 
@@ -14,8 +15,18 @@ app.use((req, res, next) => {
   });
 
   
-  app.use((req, res) => {
-    res.json({ message: "UPDATE !" }); 
- });
+  app.get('/update', async (req, res) => {
+    try {
+      const connectionString = "DRIVER={HFSQL};ANA=D:\\Sirius\\Donnees\\AGRI\\Version\\Sirius.Wd7\\Sirius.wdd;REP=D:\\Sirius\\Donnees\\AGRI\\GrpA\\Agri001\\";
+      const connection = await odbc.connect(connectionString);
+      let sql = "UPDATE CLIENTS SET Adresse1 = '7 place de la liberté' WHERE clcleunik = '68485'";
+      await connection.query(sql);
+      await connection.close();
+      res.send('La requête de mise à jour a été exécutée avec succès.');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Une erreur s\'est produite.');
+    }
+  });
 
 module.exports = app;
